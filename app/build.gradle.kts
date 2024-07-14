fd23c00ad241c88d2ec8f3684bdb4d220170da44
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2024-07-14 12:25:23 +0700

    :arrow_up: update core ktx & material

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,9 +45,9 @@ dependencies {
     val retrofitVersion: String by project
     val httpLoggingVersion: String by project
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     testImplementation("junit:junit:4.13.2")
