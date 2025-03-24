82862e2025d6ca289fb50f78e5c9018db1c7dc55
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2025-03-25 05:23:17 +0700

    :arrow_up: update gradle & kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 24
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.5"
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "$applicationId-v$versionCode($versionName)")
